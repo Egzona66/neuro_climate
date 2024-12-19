b9134dcac599b012c7491653af9d8994c360711a
Egzona Morina <egzonamorina@Egzonas-iMac.local>
2024-12-19 14:01:02 -0500

    nuni

---
+++ test.py
@@ -0,0 +1,19 @@
+print("Starting")
+
+import nibabel as nib
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+import csv
+import os
+from nilearn.datasets import fetch_icbm152_2009, fetch_atlas_harvard_oxford
+import ants
+print("AAA")
+import os
+import nibabel as nib
+import matplotlib.pyplot as plt
+import numpy as np
+from nilearn.image import resample_to_img
+
+
+print("ALl good nuni")
